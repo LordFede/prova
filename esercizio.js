f047998d3902bb18685e1de025631ef54d20e4b8
Federico Rozzo <federicorozzo@live.it>
2019-03-06 18:14:28 +0100

    primo commit

---
+++ esercizio.js
@@ -0,0 +1,84 @@
+'use strict';
+
+/*
+var vettore = [1,7,5,9,3,2,4,6,8,10];
+var scambio = true;
+var swap;
+var x =0;
+
+
+while (scambio){
+    scambio = false;
+    for (var i =0; i<(vettore.length); i++){
+        if (vettore[i] > vettore[i+1]){
+            swap = vettore[i];
+            vettore[i] = vettore[i+1];
+            vettore[i+1] = swap;
+            scambio = true;
+        }
+            
+
+    }
+}
+
+while (x < vettore.length){
+    console.log(vettore[x]);
+    x++;
+}
+
+
+var vettore = [1,7,5,9,3,2,4,6,8,10];
+var pos =0;
+var swap;
+var x =0;
+
+while(pos < vettore.length){
+
+    if (pos == 0 || vettore[pos] <= vettore[pos-1]){
+        pos +=1;
+    }
+    else{
+        swap = vettore[pos];
+        vettore[pos] = vettore[pos-1];
+        vettore[pos-1] = swap;
+        pos--;
+    }
+}
+
+
+while (x < vettore.length){
+    console.log(vettore[x]);
+    x++;
+}
+
+
+
+
+var frase = "i topi non avevano nipoti";
+var frasecorretta = frase.replace(/ /g, "");
+var ind = 0;
+var messaggio = "È palindromo";
+var dni = frasecorretta.length -1;
+
+while (ind < frasecorretta.length && ind != dni){
+    if (frasecorretta[ind] != frasecorretta[dni])
+        messaggio = "Non è palindromo";
+    ind++;
+    dni--;
+}
+
+console.log(messaggio);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
